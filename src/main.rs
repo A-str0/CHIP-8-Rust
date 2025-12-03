73e9c5218f8a35f77da976e139f32740f16755e7
A-str0 <il.eroxin@gmail.com>
2025-12-04 01:49:51 +0300

    feature/cpu: implement 5 basic instructions (`CLS`, `RET`, `SYS`, `JP`, `CALL`)

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ struct Chip8CPU {
     i: u16,                 // i registry
     pc: u16,                // program counter
     sp: u8,                 // stack pointer
+
+    delay_timer: u8,
+    sound_timer: u8,
 }
 
 const FONT: [u8; 80] = [
@@ -28,6 +31,7 @@ const FONT: [u8; 80] = [
     0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 ];
+const WORD: u16 = 2;
 
 impl Chip8CPU {
     fn new() -> Self {
@@ -36,7 +40,9 @@ impl Chip8CPU {
             stack: [0; 16],
             display: [0; 256],
             v: [0; 16], 
-            i: 0, pc: 0, sp: 0 
+            i: 0, pc: 0, sp: 0,
+            delay_timer: 0,
+            sound_timer: 0,
         };
         cpu.pc = 0x200;
         cpu.memory[0..80].copy_from_slice(&FONT);
@@ -52,6 +58,66 @@ impl Chip8CPU {
         self.memory[0x200..0x200 + rom.len()].copy_from_slice(&rom);
 
         Ok(())
+    }
+
+    fn cycle(&mut self) {
+        let oppcode = self.fetch_oppcode();
+
+        self.execute(oppcode);
+    }
+
+    fn fetch_oppcode(&self) -> u16 {
+        todo!()
+    }
+
+    fn execute(&mut self, opcode: u16) -> Result<(), String> {
+        let nnn = opcode & 0x0FFF;
+
+        // oppcode is 16 bytes so it can be separeted into 4 nibbles (полубайт, если по-русски)
+        // in our case nibble is type with four nibbles (4 bits)
+        let nibbles = (
+            ((opcode & 0xF000) >> 12) as u8,
+            ((opcode & 0x0F00) >> 8) as u8,
+            ((opcode & 0x00F0) >> 4) as u8,
+            (opcode & 0x000F) as u8,
+        );
+
+        match nibbles {
+            (0x0, 0x0, 0xE, 0x0) => self.cls(),               // CLS | clear display
+            (0x0, 0x0, 0xE, 0xE) => self.ret(),               // RET | retuern to the oppcode of stack
+            (0x0, _, _, _)       => self.sys(nnn),      // SYS | jump to machine code routine
+            (0x1, _, _, _)       => self.jp(nnn),       // JP | jump to location
+            (0x2, _, _, _)       => self.call(nnn),     //  CALL | call a subroutine
+            _ => {
+                return Err(format!("oppcode is not recognized! ({})", opcode));
+            }
+        }
+
+        return Ok(());
+    }
+
+    fn sys(&mut self, addr: u16) {
+        println!("OUTDATED!!! not in use anymore :)");
+    }
+
+    fn cls(&mut self) {
+        self.display.iter_mut().for_each(|pixel| { *pixel = 0; });
+    }
+
+    fn ret(&mut self) {
+        self.sp -= 1;
+        self.pc = self.stack[self.sp as usize];
+        self.pc += WORD;
+    }
+
+    fn jp(&mut self, addr: u16) {
+        self.pc = addr;
+    }
+
+    fn call(&mut self, addr: u16) {
+        self.stack[self.sp as usize] = self.pc;
+        self.sp += 1;
+        self.pc = addr;
     }
 }
 
