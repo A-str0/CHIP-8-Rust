5e22330e01011a7248e8815e1c579b7a4fa303ae
A-str0 <il.eroxin@gmail.com>
2025-12-04 02:30:17 +0300

    feature/cpu: implement a lot of instructions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, u8};
 
 struct Chip8CPU {
     memory: [u8; 4096],     // chip RAM
@@ -63,15 +63,21 @@ impl Chip8CPU {
     fn cycle(&mut self) {
         let oppcode = self.fetch_oppcode();
 
-        self.execute(oppcode);
+        self.execute(oppcode).expect("error lol");
     }
 
-    fn fetch_oppcode(&self) -> u16 {
-        todo!()
+    fn fetch_oppcode(&mut self) -> u16 {
+        let high = self.memory[self.pc as usize] as u16;
+        let low  = self.memory[(self.pc + 1) as usize] as u16;
+        self.pc += WORD;
+        (high << 8) | low
     }
 
     fn execute(&mut self, opcode: u16) -> Result<(), String> {
-        let nnn = opcode & 0x0FFF;
+        let nnn   = opcode & 0x0FFF;
+        let x   = ((opcode & 0x0F00) >> 8) as usize;
+        let y   = ((opcode & 0x00F0) >> 4) as usize;
+        let kk: u8     = (opcode & 0x00FF) as u8;
 
         // oppcode is 16 bytes so it can be separeted into 4 nibbles (полубайт, если по-русски)
         // in our case nibble is type with four nibbles (4 bits)
@@ -87,7 +93,11 @@ impl Chip8CPU {
             (0x0, 0x0, 0xE, 0xE) => self.ret(),               // RET | retuern to the oppcode of stack
             (0x0, _, _, _)       => self.sys(nnn),      // SYS | jump to machine code routine
             (0x1, _, _, _)       => self.jp(nnn),       // JP | jump to location
-            (0x2, _, _, _)       => self.call(nnn),     //  CALL | call a subroutine
+            (0x2, _, _, _)       => self.call(nnn),     // CALL | call a subroutine
+            (0x3, _, _, _)       => self.se_kk(x, kk),        // SE | skip next instruction if V[x] == kk
+            (0x4, _, _, _)       => self.sne_kk(x, kk),          // SNE | skip next instruction if V[x] != kk
+            (0x5, _, _, 0x0)     => self.se_y(x, y),          // SE | skip next instruction if V[x] == V[y]
+            (0x6, _, _, _)       => self.ld_ll(x, kk),           // LD | set V[x] = kk
             _ => {
                 return Err(format!("oppcode is not recognized! ({})", opcode));
             }
@@ -113,19 +123,98 @@ impl Chip8CPU {
     fn jp(&mut self, addr: u16) {
         self.pc = addr;
     }
+    fn jp_v0(&mut self, addr: u16) { 
+        self.pc = addr + self.v[0x0] as u16;
+    }
 
     fn call(&mut self, addr: u16) {
         self.stack[self.sp as usize] = self.pc;
         self.sp += 1;
         self.pc = addr;
     }
+
+    fn se_kk(&mut self, x: usize, kk: u8) {
+        if self.v[x] == kk {
+            self.pc += WORD;
+        }
+    }
+    fn se_y(&mut self, x: usize, y: usize) {
+        if self.v[x] == self.v[y] {
+            self.pc += WORD;
+        }
+    }
+    
+    fn sne_kk(&mut self, x: usize, kk: u8) {
+        if self.v[x] != kk {
+            self.pc += WORD;
+        }
+    }
+    fn sne_y(&mut self, x: usize, y: usize) {
+        if self.v[x] != self.v[y] {
+            self.pc += WORD;
+        }
+    }
+
+    fn ld_ll(&mut self, x: usize, kk: u8) {
+        self.v[x] = kk;
+    }
+    fn ld_y(&mut self, x: usize, y: usize) {
+        self.v[x] = self.v[y];
+    }
+    fn ld_i(&mut self, addr: u16) {
+        self.i = addr;
+    }
+
+    fn add_kk(&mut self, x: usize, kk: u8) {
+        let (res, overflow) = self.v[x].overflowing_add(kk);
+        self.v[0xF] = (overflow) as u8;
+        self.v[x] = res;
+    }
+    fn add_y(&mut self, x: usize, y: usize) {
+        self.add_kk(x, self.v[y]);
+    }
+
+    fn sub(&mut self, x: usize, y: usize) {
+        let (res, overflow) = self.v[x].overflowing_sub(self.v[y]);
+        self.v[x] = res;
+        self.v[0xF]= overflow as u8;
+    }
+    fn subn(&mut self, x: usize, y: usize) {
+        let (res, overflow) = self.v[y].overflowing_sub(self.v[x]);
+        self.v[x] = res;
+        self.v[0xF]= overflow as u8;
+    }
+
+    fn or(&mut self, x: usize, y: usize) {
+        self.v[x] |= self.v[y];
+    }
+
+    fn and(&mut self, x: usize, y: usize) {
+        self.v[x] &= self.v[y];
+    }
+
+    fn xor(&mut self, x: usize, y: usize) {
+        self.v[x] ^= self.v[y];
+    }
+
+    fn shr(&mut self, x: usize, y: usize) {
+        let vx = self.v[x];
+        self.v[0xF] = vx & 1;
+        self.v[x] = vx >> 1;
+    }
+
+    fn shl(&mut self, x: usize, y: usize) {
+        let vx = self.v[x];
+        self.v[0xF] = (vx >> 7) & 1;
+        self.v[x] = vx << 1;
+    }
 }
 
 fn main() {
-    let _cpu = Chip8CPU::new();
+    let mut cpu = Chip8CPU::new();
 
     let quit: bool = false;
     while quit {
-        
+        cpu.cycle();
     }
 }
