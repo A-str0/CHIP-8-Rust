d52187fa82f24d3101a7bdbf900580299e1c7e99
A-str0 <il.eroxin@gmail.com>
2025-12-07 17:39:44 +0300

    11-refactr: move main loop from `renderer.rs` to `main.rs`

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,63 +1,17 @@
-use ratatui::{init, prelude::*, restore, widgets::{Block, Borders, Paragraph} };
+use ratatui::{prelude::*, widgets::{Block, Borders, Paragraph} };
 use crate::{cpu::{Chip8CPU, DISPLAY_HEIGHT, DISPLAY_WIDTH}, sound::Chip8Sound};
 use crate::input::InputHandler;
-use std::time::{Duration, Instant};
-
-const CYCLES_PER_FRAME: u8 = 10;
-const FRAME_DURATION: Duration = Duration::from_micros(1_000_000 / TARGET_FPS);
-const TARGET_FPS: u64 = 60;
 
 pub struct Renderer {
-    cpu: Chip8CPU,
     input: InputHandler,
-    beeper: Chip8Sound,
-    quit: bool,
 }
 
 impl Renderer {
-    pub fn new(cpu: Chip8CPU, beeper: Chip8Sound) -> Self {
-        Self { cpu, input: InputHandler::new(), beeper: beeper, quit: false }
+    pub fn new() -> Self {
+        Self { input: InputHandler::new() }
     }
 
-    pub fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut terminal = init();
-        let result = self.run_loop(&mut terminal);
-        restore();
-
-        result
-    }
-
-    fn run_loop(&mut self, terminal: &mut Terminal<CrosstermBackend<std::io::Stdout>>) -> Result<(), Box<dyn std::error::Error>> {
-        let mut last_timer_update = Instant::now();
-        
-        while !self.quit {
-            let frame_start = Instant::now();
-
-            for _ in 0..CYCLES_PER_FRAME {
-                self.cpu.tick();
-                self.beeper.update(self.cpu.sound_timer);
-            }
-
-            if last_timer_update.elapsed() >= Duration::from_micros(16667) {
-                self.cpu.decrease_delay_timer();
-                self.cpu.decrease_sound_timer();
-                last_timer_update = Instant::now();
-            }
-
-            terminal.draw(|frame| self.draw(frame))?;
-            if self.input.update_keys(&mut self.cpu.keys)? {
-                self.quit = true;
-            }
-
-            let frame_time: Duration = frame_start.elapsed();
-            if frame_time < FRAME_DURATION {
-                std::thread::sleep(FRAME_DURATION - frame_time);
-            }
-        }
-        Ok(())
-    }
-
-    fn draw(&mut self, frame: &mut Frame) {
+    pub fn draw(&mut self, frame: &mut Frame, cpu: &Chip8CPU) {
         let mut lines = vec![];
 
         for row in 0..DISPLAY_HEIGHT {
@@ -65,14 +19,14 @@ impl Renderer {
             for col in 0..DISPLAY_WIDTH {
                 let byte_idx = row * 8 + (col / 8);
                 let bit_idx = 7 - (col % 8);
-                let pixel_on = (self.cpu.get_display()[byte_idx] >> bit_idx) & 1;
+                let pixel_on = (cpu.get_display()[byte_idx] >> bit_idx) & 1;
 
-                line.push(if pixel_on == 1 {'█'} else {' '});
+                line.push(if pixel_on == 1 {'▒'} else {' '});
             }
             lines.push(Line::from(line));
         }
 
-        let keys_status = format!("Keys: {:04X}", self.cpu.keys);
+        let keys_status = format!("Keys: {:04X}", cpu.keys);
 
         let paragraph = Paragraph::new(lines)
             .block(Block::default()
@@ -84,4 +38,10 @@ impl Renderer {
 
         frame.render_widget(paragraph, frame.area());
     }
+
+    pub fn should_quit(&mut self, keys: &mut u16) -> Result<bool, Box<dyn std::error::Error>> {
+        self.input.update_keys(keys)
+    }
+
+
 }
